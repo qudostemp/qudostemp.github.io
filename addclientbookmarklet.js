f44a0907cc25d5940817d31c45761c0fe7c4e42c
Rajeev Sudhakar <rajeev@qudos.com>
2015-02-12 17:35:31 -0800

    iFrame set src dynamically

--- addclientbookmarklet.js
+++ addclientbookmarklet.js
@@ -49,7 +49,86 @@
 			console.log('Selected client name = [' + client_name+ ']');
 			console.log('Source url = [' + source_url+ ']');
 
-			function iFrameStuff(s) {
+			function getClientRow(client_name) {
+		        if(typeof(client_name)==='undefined') {
+		            client_name = null;
+		        }
+
+		        if (client_name == null) {
+		            client_name = '';
+		        }
+		        
+		        return [
+		                        '            <tr class="client">',
+		                        '                <td><input type="text" id ="client-name" value="' + client_name + '"/></td>',
+		                        '                <td><input type="text" id ="client-site" value=""/></td>',
+		                        '                <td><input type="text" id ="client-industry" value=""/></td>',
+		                        '            </tr>'
+		            ].join("\n");
+		    }
+    
+		    function getClientRows(num_rows) {
+		        var client_rows = [];
+		        for (var i = 0; i < num_rows; i++)
+		        {
+		            client_rows.push(getClientRow());
+		        }
+		        
+		        return client_rows.join("\n");
+		    }
+    
+		    function getClientsForm(client_name, num_clients) {
+		        if(typeof(num_clients)==='undefined') {
+		            num_clients = 1;
+		        }
+
+		        var client_rows = [];
+		        
+		        if(client_name) {
+		            client_rows.push(getClientRow(client_name));
+		            client_rows.push(getClientRows(num_clients - 1));
+		        }
+		        else {
+		            client_rows.push(getClientRows(num_clients));
+		        }
+		        
+		        client_rows_html = client_rows.join("\n");
+		        return $([      '<form id="client-form" action="">', 
+		                        '    <table>',
+		                        '        <thead>',
+		                        '            <tr>',
+		                        '                <th>Name</th>',
+		                        '                <th>Website</th>',
+		                        '                <th>Industry</th>',
+		                        '            </tr>',
+		                        '        </thead>',
+		                        '        <tbody class="clients">',
+		                        client_rows_html,
+		                        '        </tbody>',
+		                        '    </table>',
+		                        '    <button type="submit">Submit clients</button>',
+		                        '</form>'
+		                    ].join("\n"));
+		    }
+
+			function iFrameStuff(client_name) {
+				form_html = getClientsForm(client_name, 5);
+				console.log(form_html);
+				$("body").append("\
+					<div id='wikiframe'>\
+						<div id='wikiframe_veil' style=''>\
+							<p>Loading...</p>\
+						</div>\
+						<iframe src='" + "data:text/html;charset=utf-8," + encodeURI(form_html) + "' onload=\"$('#wikiframe iframe').slideDown(500);\">Enable iFrames.</iframe>\
+						<style type='text/css'>\
+							#wikiframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
+							#wikiframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
+							#wikiframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
+						</style>\
+					</div>");			
+			}
+
+			function iFrameStuff2(s) {
 				$("body").append("\
 					<div id='wikiframe'>\
 						<div id='wikiframe_veil' style=''>\
