30e1c74808938786464e7800e0a2a04f72809060
Rajeev Sudhakar <rajeev@qudos.com>
2015-02-12 17:30:02 -0800

    Fix newlines in HTML

--- addclientbookmarklet.js
+++ addclientbookmarklet.js
@@ -50,19 +50,18 @@
 			console.log('Source url = [' + source_url+ ']');
 
 			function iFrameStuff(s) {
-				return 
-					$("body").append("
-					<div id='wikiframe'>
-						<div id='wikiframe_veil' style=''>
-							<p>Loading...</p>
-						</div>
-						<iframe src='http://en.wikipedia.org/w/index.php?&search="+s+"' onload="$('#wikiframe iframe').slideDown(500);">Enable iFrames.</iframe>
-						<style type='text/css'>
-							#wikiframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }
-							#wikiframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }
-							#wikiframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }
-						</style>
-					</div>");
+				$("body").append("\
+					<div id='wikiframe'>\
+						<div id='wikiframe_veil' style=''>\
+							<p>Loading...</p>\
+						</div>\
+						<iframe src='http://en.wikipedia.org/w/index.php?&search="+s+"' onload=\"$('#wikiframe iframe').slideDown(500);\">Enable iFrames.</iframe>\
+						<style type='text/css'>\
+							#wikiframe_veil { display: none; position: fixed; width: 100%; height: 100%; top: 0; left: 0; background-color: rgba(255,255,255,.25); cursor: pointer; z-index: 900; }\
+							#wikiframe_veil p { color: black; font: normal normal bold 20px/20px Helvetica, sans-serif; position: absolute; top: 50%; left: 50%; width: 10em; margin: -10px auto 0 -5em; text-align: center; }\
+							#wikiframe iframe { display: none; position: fixed; top: 10%; left: 10%; width: 80%; height: 80%; z-index: 999; border: 10px solid rgba(0,0,0,.5); margin: -5px 0 0 -5px; }\
+						</style>\
+					</div>");			
 			}
 
 
